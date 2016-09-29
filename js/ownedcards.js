f1cf8ff1a457efa348712c89f4e9837aa9bf07cd
db0company <db0company@gmail.com>
2016-09-29 14:47:28 -0700

    Allow to edit ownedcards with a popup on external list of owned cards

--- js/ownedcards.js
+++ js/ownedcards.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    updateOwnedCards();
+    reloadOwnedCardsAfterModal(false);
     if (('#sidebar-wrapper #id_type').length > 0 && $('#sidebar-wrapper #id_type + .cuteform').length < 1) {
 	multiCuteForms({
 	    'type': cuteformType,
--- js/profile_account_tabs.js
+++ js/profile_account_tabs.js
@@ -1,39 +1,4 @@
 
-function reloadOwnedCardsAfterModal() {
-    if (is_authenticated) {
-	$('#freeModal').on('hidden.bs.modal', function () {
-	    if (ownedcards_to_reload.length > 0) {
-		$.get('/ajax/ownedcards/?ids=' + ownedcards_to_reload.join(',') + '&page_size=' + ownedcards_to_reload.length, function(data) {
-		    var html = $(data);
-		    // Remove all that weren't returned
-		    $.each(ownedcards_to_reload, function(_, id) {
-			if (html.find('[data-ownedcard-id="' + id + '"]').length == 0) {
-			    $('[data-ownedcard-id="' + id + '"]').after('<br><br><span class="text-muted">' + gettext('Deleted') + '</span><br><br>');
-			    $('[data-ownedcard-id="' + id + '"]').remove();
-			}
-		    });
-		    html.find('.ownedcard').each(function() {
-			var newOwnedcardItem = $(this);
-			var ownedcardItem = $('.profile-account #' + newOwnedcardItem.prop('id'));
-			if (ownedcardItem.length > 0) {
-			    // Replace existing
-			    ownedcardItem.html(newOwnedcardItem.html());
-			} else {
-			    // Add at the end
-			    var account_id = newOwnedcardItem.data('ownedcard-account-id');
-			    var newElement = $('<div class="col-sm-3"></div>');
-			    newElement.html(newOwnedcardItem);
-			    $('#account' + account_id + 'Cards .row').last().append(newElement);
-			}
-		    });
-		    ownedcards_to_reload = [];
-		    updateOwnedCards();
-		});
-	    }
-	})
-    }
-}
-
 function loadAbout(pane, account) {
     pane.html('<i class="flaticon-loading"></i>');
     $.get('/ajax/account_about/' + account.data('account-id') + '/', function(data) {
